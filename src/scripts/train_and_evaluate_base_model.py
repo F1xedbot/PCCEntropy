4577721af90acf85a1d435ba57bb0b8a5b293181
F1xedbot <therealfixedbot@gmail.com>
2025-11-11 19:30:56 +0700

    feat: add temp-scaled and oof predictions

--- src/scripts/train_and_evaluate_base_model.py
+++ src/scripts/train_and_evaluate_base_model.py
@@ -9,12 +9,18 @@ import json
 import torch
 import pandas as pd
 from models.base.lightgbm_model import LightGBMClassifierModel
+from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 FEATURES_PATH = "src/data/patch_entropy.csv"
 EMBEDDINGS_PATH = "src/data/patch_embeddings.pt"
 MODEL_SAVE_PATH = "src/output/entropy_lgbm_model.pkl"
 EMBEDDINGS_SAVE_PATH = "src/data/train_split_embeddings.npz"
 RESULT_SAVE_PATH = "src/output/base_result.json"
+
+# For analysis
+OOF_PROBS_SAVE_PATH = "src/output/oof_probs.npy"
+TEMP_SCALED_PROBS_SAVE_PATH = "src/output/temp_scaled_probs.npy" 
 
 
 def main():
@@ -50,6 +56,26 @@ def main():
     if EMBEDDINGS_SAVE_PATH:
         model.save_embeddings(EMBEDDINGS_SAVE_PATH)
         print(f"Saved train/test embeddings to: {EMBEDDINGS_SAVE_PATH}")
+
+    # Get OOF Predictions and Temp Scaled Predictions for later analysis
+    # Fit a single-parameter logistic (temperature) on a validation set
+    oof_probs = model.oof_predict()
+
+    if OOF_PROBS_SAVE_PATH:
+        np.save(OOF_PROBS_SAVE_PATH, oof_probs)
+        print(f"Saved out-of-fold predictions to: {OOF_PROBS_SAVE_PATH}")
+
+    logit_probs = np.log(oof_probs / (1 - oof_probs))  # logit transform
+    temp_model = LogisticRegression(fit_intercept=False)
+    temp_model.fit(logit_probs.reshape(-1,1), model.y_train)
+
+    # Adjusted probabilities
+    logit_probs_test = np.log(oof_probs / (1 - oof_probs))
+
+    scaled_probs = 1 / (1 + np.exp(-temp_model.coef_[0][0] * logit_probs_test))
+    if TEMP_SCALED_PROBS_SAVE_PATH:
+        np.save(TEMP_SCALED_PROBS_SAVE_PATH, scaled_probs)
+        print(f"Saved temp-scaled predictions to: {TEMP_SCALED_PROBS_SAVE_PATH}")
 
 
 if __name__ == "__main__":
